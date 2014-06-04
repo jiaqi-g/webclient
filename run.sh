8f40d7755a962b9bbe91acc22e83ef7d35a3c0f7
victor2100 <fudanvictor@gmail.com>
2014-06-03 22:37:42 -0700

    add script to compile and run at one time

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "[Main] make sure you have run ./publish.sh to publish needed libs to your local repos"
+#mvn assembly:assembly -DdescriptorId=jar-with-dependencies -Dmaven.test.skip=true
+mvn package -Dmaven.test.skip=true
+cd target
+java -jar boost_client-1.0-SNAPSHOT-jar-with-dependencies.jar
